91edc8138632e6732b9bc31af72ab7f818589a3a
freeze1131 <ozenahmetcemil@gmail.com>
2026-06-01 12:15:23 +0300

    Initial Commit

---
+++ RoutineLogTests/RoutineLogTests.swift
@@ -0,0 +1,19 @@
+//
+//  RoutineLogTests.swift
+//  RoutineLogTests
+//
+//  Created by Acoman on 1.06.2026.
+//
+
+import Testing
+@testable import RoutineLog
+
+struct RoutineLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
